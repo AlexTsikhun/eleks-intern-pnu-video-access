a7e569d21b859fe4eb7d8dffed1a7375e22f4b0f
Alexander <sasacihun4@gmail.com>
2023-04-19 08:57:16 +0300

    Date filter and eliminate bags

--- src/website/filters.py
+++ src/website/filters.py
@@ -1,10 +1,14 @@
 import django_filters
+from django_filters import DateFilter
 
 from .models import Record
 
 class OrderFilter(django_filters.FilterSet):
+    # Iterate over field 'created_at'
+    start_date = DateFilter(field_name="created_at", lookup_expr="gte")
+    end_date = DateFilter(field_name="created_at", lookup_expr="lte")
+
     class Meta:
         model = Record
-        fields = '__all__'
+        fields = []
         # Don't show
-        exclude = ['brief_description', 'username', 'video_link']
